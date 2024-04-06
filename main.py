48140d3b4ddd7528bac34f4be4a17852e7591029
Mick Lynch <mick.lynch+code@gmail.com>
2024-04-06 11:26:16 -0400

    generate more output :bento:

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "Create a 7 day menu for a family of three. We want to mostly eat vegetarian and high protein. The menu should be easy to prepare and be toddler friendly",
+            "content": "Create a 7 day menu for a family of three. We want to mostly eat vegetarian and high protein. The menu should be easy to prepare and be toddler friendly. The output should be a menu, a list of ingredients and preparation instructions.",
         }
     ],
     model="mixtral-8x7b-32768",
