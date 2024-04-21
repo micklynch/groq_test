489aa0e124557fd002c6943d0516d757d04555b9
Mick Lynch <mick.lynch@gmail.com>
2024-04-21 15:01:02 -0400

    updated with Llama3-70b model

--- main.py
+++ main.py
@@ -15,12 +15,17 @@ chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "Create a 7 day menu for a family of three. We want to mostly eat vegetarian and high protein. The menu should be easy to prepare and be toddler friendly. The output should be a menu, a list of ingredients and preparation instructions.",
+            "content": "Create a 7 day menu for a family of three. We want to mostly eat vegetarian and high protein but can include meat on some days. There should be leftovers that can be used for lunch on subsequent days. The menu should be easy and quick to prepare, and be toddler friendly. The output should be a menu, a list of ingredients and preparation instructions.",
         }
     ],
-    model="mixtral-8x7b-32768",
+    # model="mixtral-8x7b-32768",
+    model="llama3-70b-8192",
 )
 
-print(chat_completion.choices[0].message.content)
+# Write the output to a markdown file
+def write_to_markdown(text, filename):
+    with open(filename, 'w') as f:
+        f.write(text)
 
-
+menu = chat_completion.choices[0].message.content
+write_to_markdown(menu, "menu.md")
